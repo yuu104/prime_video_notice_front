37236b0e138fb9816182c3c651afea184fb1dd30
yuu <yusei8171@gmail.com>
2022-10-29 17:36:01 +0900

    Next.js開発環境構築

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import styles from "@styles/pages/Home.module.scss";
+
+const Home: NextPage = () => {
+  return <div className={styles.container}>Hello Next.js</div>;
+};
+
+export default Home;
