0035c93178315a199401a7fddf0316b66bddb601
yuu <yusei8171@gmail.com>
2022-12-29 15:00:13 +0900

    refactoring

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,12 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import "../styles/globals.scss";
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
-  const redirectUri = `${process.env["NEXT_PUBLIC_BASE_URL"]}`;
-
   return (
     <Auth0Provider
       domain={process.env["NEXT_PUBLIC_AUTH0_DOMAIN"]!}
       clientId={process.env["NEXT_PUBLIC_AUTH0_CLIENT_ID"]!}
       audience={process.env["NEXT_PUBLIC_AUTH0_AUDIENCE"]!}
-      redirectUri={redirectUri}
+      redirectUri={process.env["NEXT_PUBLIC_BASE_URL"]}
     >
       <Component {...pageProps} />
     </Auth0Provider>
